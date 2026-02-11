42c3e656c5bafe26a4a9fa1544197b7cfc53bbea
Thais Palma de Brito <britop.tech@gmail.com>
2026-02-10 21:38:19 -0800

    add route to update a report to reports_blueprint.py

--- reports_blueprint.py
+++ reports_blueprint.py
@@ -77,7 +77,7 @@ def reports_index():
     except Exception as error:
         return jsonify({"error": str(error)}), 500
 
-# Read a single report - GET /hoots/<hoot_id>
+# Read a single report - GET /reports/<report_id>
 @reports_blueprint.route('/reports/<report_id>', methods=['GET'])
 def show_report(report_id):
     try:
@@ -98,6 +98,50 @@ def show_report(report_id):
             return jsonify(processed_report), 200
         else:
             connection.close()
-            return jsonify({"error": "Water report not found"}), 404
+            return jsonify({"error": "Report not found"}), 404
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
+    
+# Update a report - PUT /reports/<report_id>
+@reports_blueprint.route('/reports/<report_id>', methods=['PUT'])
+@token_required
+def update_report(report_id):
+    try:
+        updated_report_data = request.json
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("SELECT * FROM reports WHERE reports.id = %s", (report_id,))
+        report_to_update = cursor.fetchone()
+        if report_to_update is None:
+            return jsonify({"error": "Report not found"}), 404
+        connection.commit()
+        if report_to_update["author"] is not g.user["id"]:
+            return jsonify({"error": "Unauthorized"}), 401
+        cursor.execute("UPDATE reports SET title = %s, reported_at = %s, water_source = %s, water_feature = %s, location_lat = %s, location_long = %s, observation = %s, condition = %s, status = %s, image_url = %s WHERE reports.id = %s RETURNING *",
+                       (updated_report_data["title"], updated_report_data["reported_at"], updated_report_data["water_source"], updated_report_data["water_feature"], updated_report_data["location_lat"], updated_report_data["location_long"], updated_report_data["observation"], updated_report_data["condition"], updated_report_data["status"], updated_report_data["image_url"], report_id))
+        report_id = cursor.fetchone()["id"]
+        cursor.execute("""SELECT r.id, 
+                            r.author AS report_author_id, 
+                            r.title,
+                            r.reported_at, 
+                            r.water_source,
+                            r.water_feature,
+                            r.location_lat,
+                            r.location_long,
+                            r.observation,
+                            r.condition,
+                            r.status,
+                            r.image_url,
+                            r.created_at,
+                            r.updated_at, 
+                            u_report.username AS author_username
+                        FROM reports r
+                        JOIN users u_report ON r.author = u_report.id
+                        WHERE r.id = %s
+                       """, (report_id,))
+        updated_report = cursor.fetchone()
+        connection.commit()
+        connection.close()
+        return jsonify(updated_report), 200
     except Exception as error:
         return jsonify({"error": str(error)}), 500
